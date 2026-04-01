18502d0ff59bca87cbd05c63f6c9db6ca553269a
Richard Vagner <richardvagner@Richards-MBP-2.lan>
2026-03-31 17:44:36 -0700

    Richard: Fix unused useState import in drafting page

--- repos/banana-fantasy/app/drafting/page.tsx
+++ repos/banana-fantasy/app/drafting/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { useRouter } from 'next/navigation';
 import { ActiveDraftsList } from '@/components/drafting/ActiveDraftsList';
 // CompletedDraftsList moved to Standings page
